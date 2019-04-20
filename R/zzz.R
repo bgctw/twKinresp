d1a8541f190b2d8d2e3ae376a0600d0b9e0e309d
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2019-04-20 21:29:45 +0200

    current svn

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+# define to circumvent Notes for RCheck, other .tmp.f holds debugging code and will not be exported
+
+.tmp.f <- function(
+    ### wrapper to encapsulate code that should not be executed.
+){
+
+}
